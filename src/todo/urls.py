2f4fe5d51887a5bd754702edbcdfd133d35b4b23
mohamedmostafa33 <mohamedmostafaah1@gmail.com>
2026-05-18 19:06:17 +0000

    fix(todo): disable trailing slash in router for strict k8s load balancer routing & update image for new tag

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TodoViewSet, health_check
 from django.urls import path, include
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'todos', TodoViewSet, basename='todo')
 
 urlpatterns = [
