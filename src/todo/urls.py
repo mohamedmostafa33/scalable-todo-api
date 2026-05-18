6de826652cd217559a343086ad9ed118f3c55cbd
mohamedmostafa33 <mohamedmostafaah1@gmail.com>
2026-05-18 12:05:32 +0000

    feat(todo): add health check endpoint

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import TodoViewSet
+from .views import TodoViewSet, health_check
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'todos', TodoViewSet, basename='todo')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('health/', health_check, name='health-check'),
+    path('', include(router.urls)),
+]
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
 from .serializers import TodoSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Todo
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+@api_view(['GET'])
+def health_check(request):
+    return Response({
+        "status": "healthy"
+    })
